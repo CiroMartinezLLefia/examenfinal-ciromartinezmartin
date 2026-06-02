9e97d1ef46261790ac60abb0e98c0c3f15fc7f84
martinezCiro <martinezmartinciro@fpllefia.com>
2026-06-02 19:46:21 +0200

    base broken

---
+++ apps/web/src/components/EventCard.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+
+interface Props 
+    {res: { id: number, nom: string, data: Date, ciutat: string, }
+}
+
+export default function EventCard({res}: Props) {
+    return(`
+        <div className="Card">
+            <h3>${res.nom}</h3>
+            <h2>${res.ciutat}</h2>
+            <p>${res.data}</p>
+        </div>
+    `)
+}
